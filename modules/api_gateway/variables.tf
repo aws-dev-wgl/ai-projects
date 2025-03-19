2997bea15c1e4a5ff85502cc83b7261d0a8f0470
aws-dev-wgl <awsdevlabwgl@gmail.com>
2025-03-18 23:42:51 -0500

    test

--- modules/api_gateway/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "lambda_invoke_arn" {
-  description = "The ARN of the Lambda function"
-  type        = string
-}
-
-variable "lambda_function_name" {
-  description = "The name of the Lambda function"
-  type        = string
-}
