53e24af4f7d567c22dea37c1728c51a2cca6d3c7
aws-dev-wgl <awsdevlabwgl@gmail.com>
2025-03-19 00:47:12 -0500

    update

--- ROSIE-ai-powered-config-manager/terraform/main.tf
+++ ROSIE-ai-powered-config-manager/terraform/main.tf
@@ -1,64 +1,44 @@
 resource "aws_dynamodb_table" "software_inventory" {
   name         = "software-inventory"
   billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "instance_id"
+  range_key    = "software_name"
+
   attribute {
     name = "instance_id"
     type = "S"
   }
+
   attribute {
     name = "software_name"
     type = "S"
   }
-  hash_key  = "instance_id"
-  range_key = "software_name"
 }
 
 resource "aws_iam_role" "lambda_role" {
   name = "rosie_lambda_role"
 
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "lambda.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": ""
-    }
-  ]
-}
-EOF
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
 }
 
-resource "aws_iam_policy" "lambda_policy" {
-  name        = "rosie_lambda_policy"
-  description = "Policy for Lambda to access SSM, DynamoDB, and logs"
+resource "aws_iam_policy" "rosie_lambda_ssm_policy" {
+  name        = "rosie_lambda_ssm_policy"
+  description = "Policy for Lambda to access SSM, EC2, and logs"
 
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
-      {
-        Effect = "Allow"
-        Action = [
-          "dynamodb:PutItem",
-          "dynamodb:GetItem",
-          "dynamodb:Scan",
-          "dynamodb:Query"
-        ]
-        Resource = aws_dynamodb_table.software_inventory.arn
-      },
-      {
-        Effect = "Allow"
-        Action = [
-          "logs:CreateLogGroup",
-          "logs:CreateLogStream",
-          "logs:PutLogEvents"
-        ]
-        Resource = "arn:aws:logs:*:*:*"
-      },
       {
         Effect = "Allow"
         Action = [
@@ -68,17 +48,23 @@ resource "aws_iam_policy" "lambda_policy" {
           "ec2:DescribeInstances"
         ]
         Resource = "*"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ]
+        Resource = "arn:aws:logs:*:*:*"
       }
     ]
   })
 }
 
-
-
-resource "aws_iam_policy_attachment" "lambda_dynamodb" {
-  name       = "lambda_dynamodb_policy"
-  roles      = [aws_iam_role.lambda_role.name]
-  policy_arn = "arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess"
+resource "aws_iam_role_policy_attachment" "rosie_lambda_ssm_attach" {
+  policy_arn = aws_iam_policy.rosie_lambda_ssm_policy.arn
+  role       = aws_iam_role.lambda_role.name
 }
 
 resource "aws_lambda_function" "software_scan" {
@@ -87,7 +73,7 @@ resource "aws_lambda_function" "software_scan" {
   role            = aws_iam_role.lambda_role.arn
   handler         = "software_scan.lambda_handler"
   s3_bucket       = aws_s3_bucket.lambda_bucket.bucket
-  s3_key          = "software_scan.zip"
+  s3_key          = "lambda.zip"
   timeout         = 60
 
   environment {
@@ -96,7 +82,6 @@ resource "aws_lambda_function" "software_scan" {
     }
   }
 }
-
 
 resource "aws_api_gateway_rest_api" "rosie_api" {
   name        = "RosieAPI"
@@ -116,6 +101,15 @@ resource "aws_api_gateway_method" "get_software" {
   authorization = "NONE"
 }
 
+resource "aws_api_gateway_integration" "lambda_integration" {
+  rest_api_id = aws_api_gateway_rest_api.rosie_api.id
+  resource_id = aws_api_gateway_resource.software.id
+  http_method = aws_api_gateway_method.get_software.http_method
+  integration_http_method = "POST"
+  type        = "AWS_PROXY"
+  uri         = aws_lambda_function.software_scan.invoke_arn
+}
+
 resource "aws_api_gateway_deployment" "rosie_api_deployment" {
   rest_api_id = aws_api_gateway_rest_api.rosie_api.id
   depends_on  = [aws_api_gateway_integration.lambda_integration]
@@ -123,7 +117,6 @@ resource "aws_api_gateway_deployment" "rosie_api_deployment" {
   triggers = {
     redeployment = sha1(jsonencode(aws_api_gateway_rest_api.rosie_api))
   }
-
   lifecycle {
     create_before_destroy = true
   }
@@ -133,14 +126,4 @@ resource "aws_api_gateway_stage" "rosie_api_stage" {
   deployment_id = aws_api_gateway_deployment.rosie_api_deployment.id
   rest_api_id   = aws_api_gateway_rest_api.rosie_api.id
   stage_name    = "prod"
-}
-
-
-resource "aws_api_gateway_integration" "lambda_integration" {
-  rest_api_id = aws_api_gateway_rest_api.rosie_api.id
-  resource_id = aws_api_gateway_resource.software.id
-  http_method = aws_api_gateway_method.get_software.http_method
-  integration_http_method = "POST"
-  type        = "AWS_PROXY"
-  uri         = aws_lambda_function.software_scan.invoke_arn
 }
